5769565cfb6043cb61c384ece8fc6e0249a21a43
Kemylly Cavalcante <55566427+kemylly@users.noreply.github.com>
2020-08-25 10:20:41 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+﻿$('.owl-carousel').owlCarousel({
+    loop: true, /*se tem loop ou nao*/
+    margin: 10, /*margim da tela*/
+    nav: false, /* estilo da navegacao*/
+    responsive: { /*responsividade*/
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
